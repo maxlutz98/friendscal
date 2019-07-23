d15681c45aea56e2cbfe17ee49a82bf97beffdbe
Maximilian Lutz <max.lutz98@me.com>
2019-07-23 17:00:57 +0200

    Model Funktion angepasst

--- project/friendscal/models.py
+++ project/friendscal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from django.utils.translation import gettext_lazy as _
 import uuid
@@ -22,4 +23,4 @@ class Appointment(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
+        return reverse('friendscal:detail', kwargs={"pk": self.pk})
