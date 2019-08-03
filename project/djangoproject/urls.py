73b6ddc6e637f1cd00723dccdb2940ee09b49a36
Maximilian Lutz <max.lutz98@me.com>
2019-08-03 08:16:59 +0200

    admin app URLs entfernt URL Pattern für static files angepasst

--- project/djangoproject/urls.py
+++ project/djangoproject/urls.py
@@ -22,9 +22,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
     path('user/', include('django.contrib.auth.urls')),
     path('', include('friendscal.urls')),
     path('friendscal/', include('friendscal.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
